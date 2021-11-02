9387e053ac41e0f4ecadf8cbf7e087720263100e
Melih <mlhdzdr@gmail.com>
2021-11-02 04:02:14 +0300

    add sidebar and change navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,10 +30,20 @@ import MapScreen from "./screens/MapScreen";
 import DashboardScreen from "./screens/DashboardScreen";
 import SupportScreen from './screens/SupportScreen';
 import ChatBox from './components/ChatBox';
-import Navbar from "./components/NavBar/Navbar";
+import './components/NavBar/Navbar.css';
+import Dropdown from './components/NavBar/Dropdown';
+import DropdownUser from './components/NavBar/DropdownUser';
+import DropdownSeller from './components/NavBar/DropdownSeller';
+import { signout } from "./actions/userActions";
 
 
 function App() {
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+  const signoutHandler = () =>{
+    closeMobileMenu(true);
+    dispatch(signout());
+  }
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   //Değişkenleri tanımladığımız kod satırı.
@@ -44,16 +54,70 @@ function App() {
   useEffect(() => { //54.Add Category Sidebar and Filter
     dispatch(listProductCategories()); //54.Add Category Sidebar and Filter
   },[dispatch]) //54.Add Category Sidebar and Filter
+  //Navbar Dropdown Menu
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+  const [userdropdown, setUserDropdown] = useState(false);
+  const [sellerdropdown, setSellerDropdown] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onUserMouseEnter = () => {
+    if (window.innerWidth < 960) {
+        setUserDropdown(false);
+    } else {
+        setUserDropdown(true);
+    }
+  };
+
+  const onSellerMouseEnter = () => {
+    if (window.innerWidth < 960) {
+        setSellerDropdown(false);
+    } else {
+        setSellerDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  const onUserMouseLeave = () => {
+    if (window.innerWidth < 960) {
+        setUserDropdown(false);
+    } else {
+        setUserDropdown(false);
+    }
+  };
+
+  const onSellerMouseLeave = () => {
+    if (window.innerWidth < 960) {
+        setSellerDropdown(false);
+    } else {
+        setSellerDropdown(false);
+    }
+  };
   return (
     <BrowserRouter>
-    <Navbar/>
-        <div className="grid-container">
-          <aside className={sidebarIsOpen? 'open': ''}>
+        <aside className={sidebarIsOpen? 'open': ''}>
             <ul className="categories">
               <li>
                 <strong>Categories</strong>
                 <button onClick={() => setSidebarIsOpen(false)} className="clıse-sidebar" type="button">
-                  <i className="fas fa-caret-left"></i>
+                  <i className="fas fa-angle-left"></i>
                 </button> 
               </li>
               {loadingCategories ? (
@@ -66,7 +130,73 @@ function App() {
                 ))
               )}
             </ul>
-          </aside>
+        </aside>
+        <nav className='navbar'>
+          <button type="button" className="open-sidebar" onClick={() => setSidebarIsOpen(true)}>
+              <i className="fas fa-angle-right"></i>
+          </button>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            BEYOND
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+              <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+                Contact Us
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/cart' className='nav-links' onClick={closeMobileMenu}>
+                Cart {cartItems.length > 0 && (
+                      <span className="badge">{cartItems.length}</span>
+                    )}
+              </Link>
+            </li>
+            {userInfo && userInfo.isSeller && (
+              <li className='nav-item' onMouseEnter={onSellerMouseEnter} onMouseLeave={onSellerMouseLeave}>
+                  <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                  Seller<i className='fas fa-caret-down' />
+                  </Link>
+                  {sellerdropdown && <DropdownSeller />}
+              </li>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <li className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                  <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                  Admin Profile <i className='fas fa-caret-down' />
+                  </Link>
+                  {dropdown && <Dropdown />}
+              </li>
+            )}
+            <Link>
+              {userInfo ? (
+                          <li className='nav-item' onMouseEnter={onUserMouseEnter} onMouseLeave={onUserMouseLeave} >
+                              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                              User Profile <i className='fas fa-caret-down' />
+                              </Link>
+                              {userdropdown && <DropdownUser/>}
+                              <Link to='/signin' className='nav-links' onClick={signoutHandler}>
+                                  Sign Out
+                              </Link>
+                          </li>
+              ) : ( 
+                  <li className='nav-item'>
+                  <Link to='/signin' className='nav-links' onClick={closeMobileMenu}>
+                    Sign In
+                  </Link>
+                  </li>
+              )}
+            </Link>
+          </ul>
+        </nav>
+        <div className="grid-container">
           <main>
             <Route path="/seller/:id" component={SellerScreen}/>
             <Route path="/cart/:id?" component={CartScreen}/>
