9927774733ca66ffa85cdd9cd3c466589d4157f9
Melih <mlhdzdr@gmail.com>
2021-11-08 00:43:31 +0300

    update HomeCard.js

--- frontend/src/components/HomeCard.js
+++ frontend/src/components/HomeCard.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
 export default function HomeCard(props) {
     const {homeCard} = props;
     return (
         <div className="HomeCard" key={homeCard._id}>
-            <Link><img src={homeCard.homeImage} alt="homeCard" className="HomeCardImage"/></Link>
+            <img src={homeCard.homeImage} alt="homeCard" className="HomeCardImage"/>
             <div className="HomeCardDivider"></div>
             <div className="HomeCardContent">
-              <h1>{homeCard.homeHeader}</h1>
-              <p>{homeCard.homeText}</p>
+                <h1>{homeCard.homeHeader}</h1>
+                <p>{homeCard.homeText}</p>
             </div>
         </div>
     )
