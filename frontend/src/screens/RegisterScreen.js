5e360c7b2c3024dff0e287bc6d0668599fc69f11
Melih <mlhdzdr@gmail.com>
2021-11-16 01:22:41 +0300

    add register screen

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { register } from '../actions/userActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import '../screens/Styles/Register/register.css';
 
 export default function RegisterScreen(props) {
   const [name, setName] = useState('');
@@ -35,67 +36,76 @@ export default function RegisterScreen(props) {
     }
   }, [props.history, redirect, userInfo]);
   return (
-    <div>
-      <form className="form" onSubmit={submitHandler}>
-        <div>
-          <h1>Create Account</h1>
+    <div className="register">
+      <div className="headerStage">
+        <h4>Register</h4>
+        <h1>How can I join you?</h1>
+      </div>
+      <div className="mainContent">
+        <div className="formStage">
+          <form className="form" onSubmit={submitHandler}>
+            {loading && <LoadingBox></LoadingBox>}
+            {error && <MessageBox variant="danger">{error}</MessageBox>}
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                className="name"
+                placeholder="Enter name"
+                required
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                id="email"
+                className="email"
+                placeholder="Enter email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                className="password"
+                placeholder="Enter password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                className="confirmPassword"
+                placeholder="Enter confirm password"
+                required
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></input>
+            </div>
+            <br/>
+            <div className="button">
+              <button type="submit">
+                Register
+              </button>
+            </div>
+            <div>
+              <label />
+              <div>
+                Already have an account?{' '}
+                <Link to={`/signin?redirect=${redirect}`}>Sign-In</Link>
+              </div>
+            </div>
+          </form>
         </div>
-        {loading && <LoadingBox></LoadingBox>}
-        {error && <MessageBox variant="danger">{error}</MessageBox>}
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Enter name"
-            required
-            onChange={(e) => setName(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            placeholder="Enter confirm password"
-            required
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label />
-          <button className="primary" type="submit">
-            Register
-          </button>
-        </div>
-        <div>
-          <label />
-          <div>
-            Already have an account?{' '}
-            <Link to={`/signin?redirect=${redirect}`}>Sign-In</Link>
-          </div>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
