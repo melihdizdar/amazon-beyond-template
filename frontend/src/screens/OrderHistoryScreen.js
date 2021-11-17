07d96c748ddcf52f241e766802e009f5755f5a95
Melih <mlhdzdr@gmail.com>
2021-11-18 00:20:20 +0300

    add order history screen

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { listOrderMine } from '../actions/orderActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import "../screens/Styles/OrderHistory/orderhistory.css"
 
 export default function OrderHistoryScreen(props) {
   //32.display orders history
@@ -13,43 +14,47 @@ export default function OrderHistoryScreen(props) {
     dispatch(listOrderMine());
   }, [dispatch]);
   return (
-    <div>
-      <h1>Order History</h1>
+    <div className="orderhistory">
+      <div className="headerStage">
+        <h1>Order History</h1>
+      </div>
       {loading ? (
         <LoadingBox/>) : error ? (<MessageBox variant="danger">{error}</MessageBox>) : 
       (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>DATE</th>
-              <th>TOTAL</th>
-              <th>PAID</th>
-              <th>DELIVERED</th>
-              <th>ACTIONS</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => (
-              <tr key={order._id}>
-                <td>{order._id}</td>
-                <td>{order.createdAt.substring(0, 10)}</td>
-                <td>${order.totalPrice.toFixed(2)}</td>
-                <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
-                <td>
-                  {order.isDelivered? order.deliveredAt.substring(0, 10) : 'No'}
-                </td>
-                <td>
-                    <button type="button" className="small" onClick={() => { 
-                      props.history.push(`/order/${order._id}`);
-                    }}>
-                    Details
-                    </button>
-                </td>
+        <div className="tablediv">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th>ACTIONS</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>${order.totalPrice.toFixed(2)}</td>
+                  <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
+                  <td>
+                    {order.isDelivered? order.deliveredAt.substring(0, 10) : 'No'}
+                  </td>
+                  <td>
+                      <button type="button" className="small" onClick={() => { 
+                        props.history.push(`/order/${order._id}`);
+                      }}>
+                      Details
+                      </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
