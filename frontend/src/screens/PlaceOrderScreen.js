8585989d29ad4a6f93e72c237fe28bcc6ea95f34
Melih <mlhdzdr@gmail.com>
2021-11-25 00:50:47 +0300

    add place order screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -6,6 +6,7 @@ import CheckoutSteps from '../components/CheckoutSteps'
 import { ORDER_CREATE_RESET } from '../constants/orderConstants';
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import "../screens/Styles/PlaceOrder/placeorder.css"
 
 export default function PlaceOrderScreen(props) {
     const cart = useSelector((state) => state.cart); //shipping kısmındaki cart'ı tanımladık
@@ -32,94 +33,103 @@ export default function PlaceOrderScreen(props) {
     return (
         <div>
             <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
-            <div className="row top">
-                <div className="col-2">
-                    <ul>
-                        <li>
-                            <div className="card card-body">
-                                <h2>Shipping</h2>
-                                <p>
-                                    <strong>Name:</strong> {cart.shippingAddress.fullName} <br/>
-                                    <strong>Address: </strong> {cart.shippingAddress.address},{cart.shippingAddress.city},
-                                    {cart.shippingAddress.postalCode},{cart.shippingAddress.country}
-                                </p>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="card card-body">
-                                <h2>Payment</h2>
-                                <p>
-                                    <strong>Method:</strong> {cart.paymentMethod}
-                                </p>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="card card-body">
-                                <h2>Order Items</h2>
-                                <ul>
-                                    {cart.cartItems.map((item) => (
-                                            <li key={item.product}>
-                                                <div className="row">
-                                                    <div>
-                                                        <img src={item.image} alt={item.name} className="small"></img>
-                                                        {/*Sepetteki ürünün resminin çekildiği kod satırı*/}
-                                                    </div>
-                                                    <div className="min-30">
-                                                        <Link to={`/product/${item.product}`}>{item.name}</Link>
-                                                        {/*Sepetteki ürünün adının gösterildiği ve linkinin id'sine 
-                                                        göre olacağını belirleyen kod satırı*/}
-                                                    </div>
-                                                    <div>{item.qty} x ${item.price} = ${item.qty * item.price}</div>
-                                                </div>
-                                            </li>
-                                        ))}
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-1">
-                    <div className="card card-body">
+            <div className="placeorder">
+                <div className="mainContent">
+                    <div className="mainLeft">
                         <ul>
                             <li>
-                                <h2>Order Summary</h2>
-                            </li>
-                            <li>
-                                <div className="row">
-                                    <div>Items</div>
-                                    <div>${cart.itemsPrice.toFixed(2)}</div>
+                                <div className="shippingCard">
+                                    <br/>
+                                    <h2>Shipping</h2>
+                                    <p>
+                                        <strong>Name:</strong> {cart.shippingAddress.fullName} <br/>
+                                        <strong>Address: </strong> {cart.shippingAddress.address},{cart.shippingAddress.city},
+                                        {cart.shippingAddress.postalCode},{cart.shippingAddress.country}
+                                    </p>
+                                    <br/>
                                 </div>
                             </li>
                             <li>
-                                <div className="row">
-                                    <div>Shipping</div>
-                                    <div>${cart.shippingPrice.toFixed(2)}</div>
+                                <div className="paymentCard">
+                                    <br/>
+                                    <h2>Payment</h2>
+                                    <p>
+                                        <strong>Method:</strong> {cart.paymentMethod}
+                                    </p>
+                                    <br/>
                                 </div>
                             </li>
                             <li>
-                                <div className="row">
-                                    <div>Tax</div>
-                                    <div>${cart.taxPrice.toFixed(2)}</div>
+                                <div className="orderItems">
+                                    <br/>
+                                    <h2>Order Items</h2>
+                                    <ul>
+                                        {cart.cartItems.map((item) => (
+                                                <li key={item.product}>
+                                                    <div className="row">
+                                                        <div>
+                                                            <img src={item.image} alt={item.name} className="small"></img>
+                                                            {/*Sepetteki ürünün resminin çekildiği kod satırı*/}
+                                                        </div>
+                                                        <div className="min-30">
+                                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                                            {/*Sepetteki ürünün adının gösterildiği ve linkinin id'sine 
+                                                            göre olacağını belirleyen kod satırı*/}
+                                                        </div>
+                                                        <div>{item.qty} x ${item.price} = ${item.qty * item.price}</div>
+                                                    </div>
+                                                </li>
+                                            ))}
+                                    </ul>
+                                    <br/>
                                 </div>
                             </li>
-                            <li>
-                                <div className="row">
-                                    <div>
-                                        <strong>Order Total</strong>
-                                    </div>
-                                    <div>
-                                        <strong>${cart.totalPrice.toFixed(2)}</strong>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <button type="button" onClick={placeOrderHandler} className="primary block" disabled={cart.cartItems.length === 0}>
-                                    Place Order
-                                </button>
-                            </li>
-                            {loading && <LoadingBox/>}
-                            {error && <MessageBox variant="danger">{error}</MessageBox>}
                         </ul>
+                    </div>
+                    <div className="mainRight">
+                        <div className="orderSummaryCard">
+                            <ul>
+                                <li>
+                                    <h2>Order Summary</h2>
+                                </li>
+                                <li>
+                                    <div className="row">
+                                        <div><strong>Items</strong></div>
+                                        <div>${cart.itemsPrice.toFixed(2)}</div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="row">
+                                        <div><strong>Shipping</strong></div>
+                                        <div>${cart.shippingPrice.toFixed(2)}</div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="row">
+                                        <div><strong>Tax</strong></div>
+                                        <div>${cart.taxPrice.toFixed(2)}</div>
+                                    </div>
+                                </li>
+                                <div className="line"/>
+                                <li>
+                                    <div className="row">
+                                        <div>
+                                            <strong>Order Total</strong>
+                                        </div>
+                                        <div>
+                                            <strong>${cart.totalPrice.toFixed(2)}</strong>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <button type="button" onClick={placeOrderHandler} className="primary block" disabled={cart.cartItems.length === 0}>
+                                        Place Order
+                                    </button>
+                                </li>
+                                {loading && <LoadingBox/>}
+                                {error && <MessageBox variant="danger">{error}</MessageBox>}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
