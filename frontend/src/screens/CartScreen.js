6534ad14b28bd9051a51bb3d8e1338ed78b34f12
Melih <mlhdzdr@gmail.com>
2021-11-24 19:59:54 +0300

    add cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { addToCart, removeFromCart } from '../actions/cardActions';
 import MessageBox from '../components/MessageBox';
+import "../screens/Styles/Cart/cart.css";
 
 export default function CartScreen(props) {
     /*Kullanılacak değerlerin tanımlandığı yer.*/
@@ -27,69 +28,81 @@ export default function CartScreen(props) {
         //Procced to Checkout'a bastığı zaman olacak aksiyon.
     };
     return (
-        <div className="row top">
-            <div className="col-2">
+        <div className="cart">
+            <div className="headerStage">
                 <h1>Shopping Cart</h1>
-                {error && <MessageBox variant="danger">{error}</MessageBox>}
-                {cartItems.length === 0 ? (<MessageBox>
-                    Cart is empty. <Link to="/">Go Shopping</Link>
-                </MessageBox>
-                //sepet boş olduğu zaman vereceği mesaj
-                ):(
-                    <ul>
-                        {
-                            cartItems.map((item) => (
-                                <li key={item.product}>
-                                    <div className="row">
-                                        <div>
-                                            <img src={item.image} alt={item.name} className="small"></img>
-                                            {/*Sepetteki ürünün resminin çekildiği kod satırı*/}
-                                        </div>
-                                        <div className="min-30">
-                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
-                                            {/*Sepetteki ürünün adının gösterildiği ve linkinin id'sine 
-                                            göre olacağını belirleyen kod satırı*/}
-                                        </div>
-                                        <div>
-                                            <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
-                                                {
-                                                    [...Array(item.coutInStock).keys()].map((x) => (
-                                                        <option key={x+1} value={x+1}>{x+1}</option>
-                                                    )
-                                                    //Sepetteki ürünün qty kısmının çekildiği kod satırı
-                                                )}
-                                            </select>
-                                        </div>
-                                        <div>${item.price}</div> {/*Sepetteki ürünün fiyatının çekildiği kod satırı*/}
-                                        <div>
-                                            <button type="button" onClick={() => removeFromCartHandler(item.product)}>
-                                                Delete
-                                            </button>
-                                            {/*Sepetteki ürünlerin silme işleminin yapıldığı button*/}
-                                        </div>
-                                    </div>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                )
-                }
             </div>
-            <div className="col-1">
-                <div className="card card-body">
-                    <ul>
-                        <li>
-                        <h2>
-                            Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
-                            {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
-                        </h2>
-                        </li>
-                        <li>
-                            <button type="button" onClick={checkoutHandler} className="primary block" disabled={cartItems.length === 0}>
-                                Procced to Checkout
-                            </button>
-                        </li>
-                    </ul>
+            <div className="left">
+                <div className="leftContent">
+                    {error && <MessageBox variant="danger">{error}</MessageBox>}
+                    {cartItems.length === 0 ? (<MessageBox>
+                        Cart is empty. <Link to="/">Go Shopping</Link>
+                    </MessageBox>
+                    //sepet boş olduğu zaman vereceği mesaj
+                    ):(
+                        <div className="scroll-bg">
+                            <div className="scroll-div">
+                                <div className="scroll-object">
+                                    <ul>
+                                        {
+                                            cartItems.map((item) => (
+                                                <li key={item.product}>
+                                                    <div className="row">
+                                                        <div>
+                                                            <img src={item.image} alt={item.name} className="small"></img>
+                                                            {/*Sepetteki ürünün resminin çekildiği kod satırı*/}
+                                                        </div>
+                                                        <div className="min-30">
+                                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                                            {/*Sepetteki ürünün adının gösterildiği ve linkinin id'sine 
+                                                            göre olacağını belirleyen kod satırı*/}
+                                                        </div>
+                                                        <div>
+                                                            <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                                                                {
+                                                                    [...Array(item.coutInStock).keys()].map((x) => (
+                                                                        <option key={x+1} value={x+1}>{x+1}</option>
+                                                                    )
+                                                                    //Sepetteki ürünün qty kısmının çekildiği kod satırı
+                                                                )}
+                                                            </select>
+                                                        </div>
+                                                        <div>${item.price}</div> {/*Sepetteki ürünün fiyatının çekildiği kod satırı*/}
+                                                        <div>
+                                                            <button type="button" onClick={() => removeFromCartHandler(item.product)}>
+                                                                Delete
+                                                            </button>
+                                                            {/*Sepetteki ürünlerin silme işleminin yapıldığı button*/}
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                    }
+                </div>
+            </div>
+            <div className="right">
+                <div className="rightStage">
+                    <div className="colCard">
+                        <ul>
+                            <li>
+                            <h2>
+                                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
+                                {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+                            </h2>
+                            </li>
+                            <li>
+                                <button type="button" onClick={checkoutHandler} className="primary block" disabled={cartItems.length === 0}>
+                                    Procced to Checkout
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
